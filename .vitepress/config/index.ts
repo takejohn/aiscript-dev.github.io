2e8875310219fd926bafa108f4852b01e77d8d3f
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-11-29 11:52:50 +0900

    feat(seo): canonical url

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -1,12 +1,47 @@
 import { defineConfig } from 'vitepress';
+import type { LocaleConfig, DefaultTheme, HeadConfig } from 'vitepress';
 import { shared } from './shared';
 import { ja } from './ja';
 import { en } from './en';
 
+const locales: LocaleConfig<DefaultTheme.Config> = {
+    ja: { label: '日本語', ...ja },
+    en: { label: 'English', ...en },
+};
+
+const baseUrl = 'https://aiscript-dev.github.io';
+
 export default defineConfig({
     ...shared,
-    locales: {
-        ja: { label: '日本語', ...ja },
-        en: { label: 'English', ...en },
+    locales,
+    transformHead(context) {
+        const head: HeadConfig[] = [ ...context.head ];
+        if (!context.pageData.isNotFound) {
+            const localesRegex = new RegExp(`^/(${Object.keys(locales).join('|')})`);
+            const canonical = '/' + context.page.replace(/(index)?\.md$/, '');
+    
+            for (const locale of Object.keys(locales)) {
+                const localePath = canonical.replace(localesRegex, `/${locale}`);
+    
+                head.push([
+                    'link',
+                    {
+                        rel: 'alternate',
+                        hreflang: locales[locale].lang || locale,
+                        href: baseUrl + localePath,
+                    },
+                ]);
+            }
+    
+            head.push([
+                'link',
+                {
+                    rel: 'canonical',
+                    href: baseUrl + canonical,
+                }
+            ]);    
+        }
+
+        return head;
     },
 });
