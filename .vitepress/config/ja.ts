c9a1d6f17b3b3e70ad7fab8671d2cff2431d7e67
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-12-06 09:50:37 +0900

    refactor

--- .vitepress/config/ja.ts
+++ .vitepress/config/ja.ts
@@ -27,6 +27,28 @@ const referenceNav: DefaultTheme.SidebarItem[] = [
     { text: '組み込み関数（Math）', link: 'std-math' },
 ];
 
+export const jaSearchLocale: NonNullable<DefaultTheme.LocalSearchOptions['translations']> = {
+    button: {
+        buttonText: '検索',
+        buttonAriaLabel: '検索する',
+    },
+    modal: {
+        displayDetails: '詳細を表示',
+        resetButtonTitle: 'リセット',
+        backButtonTitle: '閉じる',
+        noResultsText: '該当する結果がありませんでした',
+        footer: {
+            selectText: 'で選択',
+            selectKeyAriaLabel: 'Enterキー',
+            navigateText: 'で移動',
+            navigateUpKeyAriaLabel: '上矢印キー',
+            navigateDownKeyAriaLabel: '下矢印キー',
+            closeText: 'で閉じる',
+            closeKeyAriaLabel: 'Escキー',
+        },
+    },
+};
+
 export const ja = defineConfig({
     lang: 'ja-JP',
     description: 'ブラウザで動く、ユーザースクリプト用言語',
@@ -56,25 +78,3 @@ export const ja = defineConfig({
         returnToTopLabel: 'ページの先頭に戻る',    
     },
 });
-
-export const jaSearchLocale: NonNullable<DefaultTheme.LocalSearchOptions['translations']> = {
-    button: {
-        buttonText: '検索',
-        buttonAriaLabel: '検索する',
-    },
-    modal: {
-        displayDetails: '詳細を表示',
-        resetButtonTitle: 'リセット',
-        backButtonTitle: '閉じる',
-        noResultsText: '該当する結果がありませんでした',
-        footer: {
-            selectText: 'で選択',
-            selectKeyAriaLabel: 'Enterキー',
-            navigateText: 'で移動',
-            navigateUpKeyAriaLabel: '上矢印キー',
-            navigateDownKeyAriaLabel: '下矢印キー',
-            closeText: 'で閉じる',
-            closeKeyAriaLabel: 'Escキー',
-        },
-    },
-};
