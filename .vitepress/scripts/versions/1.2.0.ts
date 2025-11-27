381550d8ba1f2d5a2b860ba772aead9b83e472aa
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-11-27 17:12:35 +0900

    update deps

---
+++ .vitepress/scripts/versions/1.2.0.ts
@@ -0,0 +1,54 @@
+import { AISCRIPT_VERSION, Parser, Interpreter, utils, errors, type Ast } from 'aiscript1_2';
+import { Runner } from '../runner';
+
+export default class extends Runner {
+    version = AISCRIPT_VERSION;
+
+    parse(code: string) {
+        const ast = Parser.parse(code);
+        const metadata = Interpreter.collectMetadata(ast);
+        return [ast, metadata] as const;
+    }
+
+    private interpreter = new Interpreter({}, {
+        out: (value) => {
+            this.print(
+                value.type === 'num' ? value.value.toString()
+                : value.type === 'str' ? `"${value.value}"`
+                : JSON.stringify(utils.valToJs(value), null, 2) ?? '',
+            );
+        },
+        log: (type, params) => {
+            if (type === 'end' && params.val != null && 'type' in params.val) {
+                this.print(utils.valToString(params.val, true));
+            }
+        },
+    });
+    async exec(node: unknown): Promise<void> {
+        await this.interpreter.exec(node as Ast.Node[]);
+    }
+    isAiScriptError(error: unknown): error is errors.AiScriptError {
+        return error instanceof errors.AiScriptError;
+    }
+    getErrorName(error: errors.AiScriptError): string | undefined {
+        if (error instanceof errors.AiScriptSyntaxError) {
+            return 'SyntaxError';
+        }
+        if (error instanceof errors.AiScriptTypeError) {
+            return 'TypeError';
+        }
+        if (error instanceof errors.AiScriptRuntimeError) {
+            return 'RuntimeError';
+        }
+        if (error instanceof errors.AiScriptIndexOutOfRangeError) {
+            return 'IndexOutOfRangeError';
+        }
+        if (error instanceof errors.AiScriptUserError) {
+            return 'UserError';
+        }
+        return 'AiScriptError';
+    }
+    dispose() {
+        this.interpreter.abort();
+    }
+}
